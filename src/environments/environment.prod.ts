56708dcc77b638d115623b00b4ccfb0f184f0821
milan.zivanov <milan.zivanov@gmail.com>
2019-05-08 17:56:30 +0200

    adding firebase adding some code to pollyfilles.ts for global issue and in index.html commented anoteh solution

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAxffEyNtVM1kGqtYMv_D-3xGsn93uRQDc",
+    authDomain: "oshop-2f3a0.firebaseapp.com",
+    databaseURL: "https://oshop-2f3a0.firebaseio.com",
+    projectId: "oshop-2f3a0",
+    storageBucket: "oshop-2f3a0.appspot.com",
+    messagingSenderId: "1070057134321",
+    appId: "1:1070057134321:web:6d0bfa83d15875d8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAxffEyNtVM1kGqtYMv_D-3xGsn93uRQDc",
+    authDomain: "oshop-2f3a0.firebaseapp.com",
+    databaseURL: "https://oshop-2f3a0.firebaseio.com",
+    projectId: "oshop-2f3a0",
+    storageBucket: "oshop-2f3a0.appspot.com",
+    messagingSenderId: "1070057134321",
+    appId: "1:1070057134321:web:6d0bfa83d15875d8"
+  }
 };
 
 /*
